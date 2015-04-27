48d5682e8f06667792583c3d10dd0588622a35a8
George <georgi_gbalabanov@abv.bg>
2015-04-27 18:20:25 +0300

    modified GHInfo class

--- Week6/requesting.py
+++ Week6/requesting.py
@@ -7,23 +7,32 @@ class GHInfo:
     URL_followers = "https://api.github.com/users/?/followers"
     URL_following = "https://api.github.com/users/?/following"
     header = {"client_id": "e7fa7529c9af0e814361",
-              "client_secret": "a6526713d18d647258be57feb17fe727db1a4901"}
+              "client_secret": "a6526713d18d647258be57feb17fe727db1a4901",
+              "page": ""}
 
     def _get_followers(self, name):
         followers = []
         try:
-            for x in requests.get(GHInfo.URL_followers.replace("?",name), params=GHInfo.header).json():
-                followers.append(x["login"])
-            return followers
+            temp = requests.get(GHInfo.URL.replace("?", name), params = GHInfo.header).json()
+            count = (temp["followers"] // 30) + 1
+            for page in range(1, count + 1):
+                GHInfo.header["page"] = page
+                for x in requests.get(GHInfo.URL_followers.replace("?", name), params=GHInfo.header).json():
+                    followers.append(x["login"])
+            return followers, len(followers)
         except TypeError:
             print("No such account !")
 
     def _get_following(self, name):
         following = []
         try:
-            for x in requests.get(GHInfo.URL_following.replace("?",name), params=GHInfo.header).json():
-                following.append(x["login"])
-            return following
+            temp = requests.get(GHInfo.URL.replace("?", name), params = GHInfo.header).json()
+            count = (temp["following"] // 30) + 1
+            for page in range(1, count + 1):
+                GHInfo.header["page"] = page
+                for x in requests.get(GHInfo.URL_following.replace("?", name), params=GHInfo.header).json():
+                    following.append(x["login"])
+            return following, len(following)
         except TypeError:
             print("No such account !")
 
@@ -32,4 +41,5 @@ class GHInfo:
         self._following = self._get_following(acc_name)
 
 
-g = GHInfo("hristozov")
+g = GHInfo("radorado")
+print(g._following)
